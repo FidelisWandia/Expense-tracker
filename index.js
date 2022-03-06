ddc8b91b5232cf69b8ddb26063475268d1642b16
FidelisWandia <fidelismundia2000@gmail.com>
2022-03-06 08:43:48 +0300

    tracks expense

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-let expenseArr = []
-let tableArr=[]
+
 //DOM
 const addBtn = document.getElementById("add-btn")
 let inputName = document.getElementById("input-name")
 let inputDate = document.getElementById("input-date")
 let inputAmt = document.getElementById("input-amt")
-//let cells = document.querySelectorAll("table tr td:nth-child(2)")
-let expenseRaw = document.getElementById("expense-raw")
-console.log(expenseRaw)
+let table = document.getElementById("myTable")
 
 //Add expense btn
 addBtn.addEventListener("click", function(){
-    expenseArr.push(inputName.value)
-    expenseArr.push(inputDate.value)
-    expenseArr.push(inputAmt.value)
-    let listItems=""
-for (let i = 0; i < expenseArr.length; i++){
-    listItems=`<td>${expenseArr[i]}</td>`
-}
-listItems.innerHTML=listItems
-
+    let expenseRow = table.insertRow(1)
+    let cellName = expenseRow.insertCell(0)
+    let cellDate = expenseRow.insertCell(1)
+    let cellAmount = expenseRow.insertCell(2)
+    cellName.innerHTML = inputName.value 
+    cellDate.innerHTML = inputDate.value 
+    cellAmount.innerHTML = inputAmt.value
 })
-
-/*for(let i = 0; i < cells.length; i++){
-    cells[i].innerHTML = expenseArr
-}*/
-
-/*let listItems=""
-for (let i = 0; i < expenseArr.length; i++){
-    listItems=`<td>${expenseArr[i]}</td>`
-}
-listItems.innerHTML=listItems*/  
-
-
 
 
 
